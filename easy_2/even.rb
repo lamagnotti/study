77680a0ed799ab96669549c18f9741315fb763d4
Louis Magnotti <magnottila@gmail.com>
2016-07-24 14:17:01 -0400

    Revert "Completed second exercise set with explainations"
    This reverts commit cca75aba316f69b92f7824f445606ff9b54083e5.

--- easy_2/even.rb
+++
@@ -1,8 +0,0 @@
-# Using a while loop, count up from 1..99 by 1's, and print out each value. In this case
-# only the even numbers are being printed because we're calling #even? on the value (counter).
-
-value = 1
-while value <= 99
-  puts value if value.even?
-  value += 1
-end
